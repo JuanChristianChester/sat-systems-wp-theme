444ad57473fa10059dfe56bbf7b91267f01a24c7
Juan Christian <Juanchristian2002@outlook.com>
2023-05-13 12:55:44 +0100

    Default image
    Adding in conditionals to display default image when none is selected

--- header.php
+++ header.php
@@ -64,7 +64,12 @@ use Digital_Newspaper\CustomizerDefault as DN;
 		<?php elseif (is_category()) : ?>
 			<h1 class="header-text"><?php echo single_cat_title(); ?></h1>
 		<?php endif; ?>
-		<img class="header-image" src="<?php echo get_field("header_img"); ?>" alt="Rack" width="500" height="600" />
+		<!-- if get_field(header_img) is null, return a default image -->
+		<?php if (get_field("header_img") == null) : ?>
+			<img class="header-image" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/custom-images/clownfish.png" alt="header-image" width="500" height="600" />
+		<?php else: ?>
+		<img class="header-image" src="<?php echo get_field("header_img"); ?>" alt="header-image" width="500" height="600" />
+		<?php endif; ?>
 		<?php
 		/**
 		 * function - digital_newspaper_after_header_html

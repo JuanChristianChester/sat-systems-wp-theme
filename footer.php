515025a7096cc98c96bbfb3dc69150652871e9cd
ItchyPrune <94751482+ItchyPrune@users.noreply.github.com>
2023-05-18 11:59:17 +0100

    Footer links (test) using php echo home url instead of hardcoding

--- footer.php
+++ footer.php
@@ -16,84 +16,85 @@
  */
 do_action('digital_newspaper_before_footer_section');
 ?>
-	<footer id="colophon" class="site-footer dark_bk">
-		
-		<div class="digital-newspaper-container" id="footerNewsPaperContainer">
-			<div class="row" id="certifiedRow">
-				<h2 id="certifiedFooterText">Certified</h2>
+<footer id="colophon" class="site-footer dark_bk">
+
+	<div class="digital-newspaper-container" id="footerNewsPaperContainer">
+		<div class="row" id="certifiedRow">
+			<h2 id="certifiedFooterText">Certified</h2>
+		</div>
+	</div>
+	<div class="digital-newspaper-container" id="footerNewsPaperContainer">
+		<div class="row" id="certifiedLogosContainer">
+			<div class="certification-logo">
+				<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/JOSCAR_PNG_Format-removebg-preview.png"; ?>>
+			</div>
+			<div class="certification-logo">
+				<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/RGB_IMCA_Logo_-_Supplier__S__Member-removebg-preview.png"; ?>>
+			</div>
+			<div class="certification-logo">
+				<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/SGS_ISO_9001_TCL_HR-removebg-preview.png"; ?>>
 			</div>
 		</div>
-		<div class="digital-newspaper-container" id="footerNewsPaperContainer">
-			<div class="row" id="certifiedLogosContainer">
-				<div class="certification-logo">
-					<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/JOSCAR_PNG_Format-removebg-preview.png"; ?>>
-				</div>
-				<div class="certification-logo">
-					<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/RGB_IMCA_Logo_-_Supplier__S__Member-removebg-preview.png"; ?>>
-				</div>
-				<div class="certification-logo">
-					<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/SGS_ISO_9001_TCL_HR-removebg-preview.png"; ?>>
-				</div>
-			</div>
+	</div>
+	<div class="digital-newspaper-container" id="footerNewsPaperContainer">
+		<div class="row" id="ltdRow">
+			<h2 id="ltdFooterText">Sub Aquatic Technical Systems Ltd</h2>
 		</div>
-		<div class="digital-newspaper-container" id="footerNewsPaperContainer">
-			<div class="row" id="ltdRow">
-				<h2 id="ltdFooterText">Sub Aquatic Technical Systems Ltd</h2>
-			</div>
-		</div>
-		<div class="digital-newspaper-container" id="footerNewsPaperContainer">
-			<div class="footer-inner digital-newspaper-container">
-				<div class="row">
-					<div class="footer-widget column-three">
-						<ul>
-							<a>Home</a>
-							<a>Helpdesk</a>
-							<a>News</a>
-							<a>Contact</a>
-							<a>About</a>
-						</ul>
-					</div>
-					<div class="footer-widget column-three">
-					<ul>
-							<a>Products and Solutions</a>
-							<a>Instrumentation</a>
-							<a>Data Logging</a>
-							<a>Communication</a>
-						</ul>
-					</div>
-					<div class="footer-widget column-three">
-					<ul>
-							<a>Industries</a>
-							<a>Commercial Diving</a>
-							<a>Medical</a>
-							<a>Defense</a>
-							<a>Tunneling</a>
-							<a>Leisure Submarines</a>
-							<a>Hyperbaric Chambers</a>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="digital-newspaper-container" id="satSysTriangle">
-			<div class="footer-inner digital-newspaper-container">
+	</div>
+	<div class="digital-newspaper-container" id="footerNewsPaperContainer">
+		<div class="footer-inner digital-newspaper-container">
+			<div class="row">
 				<div class="footer-widget column-three">
-					<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/SAT_Triangle_Logo-removebg-preview.png"?>>
+					<ul>
+						<a href="<?php echo home_url(); ?>">Home</a>
+						<a href="<?php echo home_url('/helpdesk'); ?>">Helpdesk</a>
+						<a href="<?php echo home_url('/category/news'); ?>">News</a>
+						<a href="<?php echo home_url('/contact-us-2'); ?>">Contact</a>
+						<a href="<?php echo home_url('/about-2'); ?>">About</a>
+					</ul>
+				</div>
+
+				<div class="footer-widget column-three">
+					<ul>
+						<a>Products and Solutions</a>
+						<a>Instrumentation</a>
+						<a>Data Logging</a>
+						<a>Communication</a>
+					</ul>
+				</div>
+				<div class="footer-widget column-three">
+					<ul>
+						<a>Industries</a>
+						<a>Commercial Diving</a>
+						<a>Medical</a>
+						<a>Defense</a>
+						<a>Tunneling</a>
+						<a>Leisure Submarines</a>
+						<a>Hyperbaric Chambers</a>
+					</ul>
 				</div>
 			</div>
 		</div>
-		<div class="digital-newspaper-container" id="footerBottomSection">
-			<div class="footer-inner digital-newspaper-container">
-				<div class="footer-widget column-three" id="locationRow">
-					<p>SAT Systems Ltd, 26 Main Road, Kirk Michael, Isle of Man, IM6 1SS</p>
-				</div>
-				<div class="footer-widget column-three" id="policiesRow">
-						<ul>
-							<a>Terms & Conditions</a>
-							<a>Privacy Policy</a>
-							<a>Quality Policy</a>
-						</ul>
-				</div>
+	</div>
+	<div class="digital-newspaper-container" id="satSysTriangle">
+		<div class="footer-inner digital-newspaper-container">
+			<div class="footer-widget column-three">
+				<img src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/SAT_Triangle_Logo-removebg-preview.png" ?>>
+			</div>
+		</div>
+	</div>
+	<div class="digital-newspaper-container" id="footerBottomSection">
+		<div class="footer-inner digital-newspaper-container">
+			<div class="footer-widget column-three" id="locationRow">
+				<p>SAT Systems Ltd, 26 Main Road, Kirk Michael, Isle of Man, IM6 1SS</p>
+			</div>
+			<div class="footer-widget column-three" id="policiesRow">
+				<ul>
+					<a>Terms & Conditions</a>
+					<a>Privacy Policy</a>
+					<a>Quality Policy</a>
+				</ul>
+			</div>
 			<ul class="social-icons-footer">
 				<a>
 					<img class="footer-social-icons" src=<?php echo get_stylesheet_directory_uri() . "/assets/images/custom-images/Twitter.png"; ?>>

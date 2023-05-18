3c2854ef1d6a3a76246eef48301646716897a4ad
ItchyPrune <94751482+ItchyPrune@users.noreply.github.com>
2023-05-18 12:59:32 +0100

    Final custom.js change

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var container = document.getElementById('product-quantity-container');
       var addButton = document.getElementById('add-more');
 
-      addButton.addEventListener('click', function() {
+      addButton.addEventListener('click', function() { 
         var productField = document.createElement('select');
         productField.name = 'products[]';
         productField.innerHTML = '<option value="">Select a product</option><option value="C01-001-001-02-0-1 - Product 1">C01-001-001-02-0-1 - Product 1</option><option value="SAT-30-01 - Product 2">SAT-30-01 - Product 2</option><option value="C08-001-001-02-1-1 - Product 3">C08-001-001-02-1-1 - Product 3</option><option value="C08-012-013-01-2-1 - Product 4">C08-012-013-01-2-1 - Product 4</option><option value="B09-35-13 - Product 5">B09-35-13 - Product 5</option>';
@@ -12,41 +12,27 @@ document.addEventListener('DOMContentLoaded', function() {
         var quantityField = document.createElement('input');
         quantityField.type = 'number';
         quantityField.name = 'quantities[]';
-        quantityField.min = '1';
-        quantityField.classList.add('quantity-field');
+        quantityField.classList.add('quantity-field'); // Add a CSS class to the quantity field
 
         var removeButton = document.createElement('button');
         removeButton.type = 'button';
-        removeButton.classList.add('remove-button');
-        removeButton.textContent = 'Remove';
-        removeButton.addEventListener('click', function() {
-          container.removeChild(wrapper);
-        });
+        removeButton.innerText = 'Remove';
+        removeButton.classList.add('remove-button'); // Add a CSS class to the remove button
 
         var wrapper = document.createElement('div');
-        wrapper.classList.add('product-quantity-wrapper');
+        wrapper.classList.add('product-quantity-wrapper'); // Add a CSS class to the wrapper div
         wrapper.appendChild(productField);
         wrapper.appendChild(quantityField);
         wrapper.appendChild(removeButton);
 
         container.appendChild(wrapper);
-      });
+    });
 
-      var form = document.querySelector('form');
-      form.addEventListener('submit', function() {
-        var products = document.querySelectorAll('.product-field');
-        var quantities = document.querySelectorAll('.quantity-field');
-        var productsQuantities = [];
-
-        for (var i = 0; i < products.length; i++) {
-          var product = products[i].value;
-          var quantity = quantities[i].value;
-          if (product !== '' && quantity !== '') {
-            productsQuantities.push(product + ' - ' + quantity);
-          }
+    container.addEventListener('click', function(e) {
+        if (e.target.classList.contains('remove-button')) {
+            e.target.parentNode.remove();
         }
+    });
+}
+});
 
-        document.getElementById('products-quantities').value = productsQuantities.join('\n');
-      });
-    }
-  });
